0b12b0c26fb6e274becd401799cc1746ce3929fc
Allesson <allessoncavalcante@gmail.com>
2016-05-19 15:01:48 -0300

    adicionando banco

--- app/src/main/java/br/com/guiamobile/model/PontoTuristicoOpenHelper.java
+++ app/src/main/java/br/com/guiamobile/model/PontoTuristicoOpenHelper.java
@@ -29,14 +29,23 @@ public class PontoTuristicoOpenHelper extends SQLiteOpenHelper {
         db.execSQL(SQL_TBL_PONTO_BARESR);
 
         //Inserindo os Campos..
-        db.execSQL(MUSEUS);
         db.execSQL(MARCO_ZERO);
         db.execSQL(PONTES);
-        db.execSQL(MERCADOS);
         db.execSQL(ARTESANATOS);
         db.execSQL(PARQUES);
         db.execSQL(PRAIAS);
         db.execSQL(BARES);
+
+        db.execSQL(M_A_M_A_MAGALHAES);
+        db.execSQL(M_C_RECIFE);
+        db.execSQL(M_H_NORDESTE);
+        db.execSQL(M_M_GRECA);
+        db.execSQL(M_MILITAR);
+
+        db.execSQL(ME_B_VISTA);
+        db.execSQL(ME_MADALENA);
+        db.execSQL(ME_S_JOSE);
+
     }
 
     @Override
@@ -51,7 +60,10 @@ public class PontoTuristicoOpenHelper extends SQLiteOpenHelper {
                     "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
                     "titulo TEXT," +
                     "descricao TEXT," +
-                    "texto TEXT" +
+                    "texto TEXT," +
+                    "latitude REAL," +
+                    "longitude REAL," +
+                    "idTab INTEGER" +
                     ");";
     // ----------------------------------------------------
 
@@ -84,7 +96,10 @@ public class PontoTuristicoOpenHelper extends SQLiteOpenHelper {
                     "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
                     "titulo TEXT," +
                     "descricao TEXT," +
-                    "texto TEXT" +
+                    "texto TEXT," +
+                    "latitude REAL," +
+                    "longitude REAL," +
+                    "idTab INTEGER" +
                     ");";
     // ----------------------------------------------------
 
@@ -145,12 +160,6 @@ public class PontoTuristicoOpenHelper extends SQLiteOpenHelper {
 
     // ----------------------------------------------------
     //01
-    private static final String MUSEUS =
-            "INSERT INTO tbl_pontoMuseusT (titulo,descricao,texto)" +
-                    "values ('MUseus e Teatros'," +
-                    "'Museus eeeee'," +
-                    "'Museussssssssss'" +
-                    ");";
 
     // ----------------------------------------------------
     //01
@@ -162,13 +171,7 @@ public class PontoTuristicoOpenHelper extends SQLiteOpenHelper {
                     ");";
 
     // ----------------------------------------------------
-    //01
-    private static final String MERCADOS =
-            "INSERT INTO tbl_pontoMercados (titulo,descricao,texto)" +
-                    "values ('MERCADOS'," +
-                    "'Mercados'," +
-                    "'Mercadossssssssss'" +
-                    ");";
+
 
     // ----------------------------------------------------
     //01
@@ -207,5 +210,64 @@ public class PontoTuristicoOpenHelper extends SQLiteOpenHelper {
                     ");";
 
     // ----------------------------------------------------
+
+    //MUSEUS______________________________________________________________________________________________
+
+    private static final String M_H_NORDESTE =
+            "INSERT INTO tbl_pontoMuseusT (titulo,descricao,texto, latitude, longitude, idTab)" +
+                    "values ('Museu do Homem do Nordeste'," +
+                    "'Av. Dezessete de Agosto,2187, Casa Forte.'," +
+                    "' É um órgão federal (vinculado à Fundação Joaquim Nabuco/Ministério da Educação), que reúne acervos que revelam a pluralidade das culturas negras, indígenas e brancas desde nossas origens até os diferentes desdobramentos e misturas que formam o que hoje é chamado genericamente de cultura brasileira. Esses acervos servem de suporte para construir narrativas que estão traduzidas em exposições etnográficas e exposições de arte, assim como em ações educativas de mediação cultural e em diferentes eventos que compõem a programação cultural do museu.'," +"-8.030196,"+"-34.925281,"+"1"+
+            ");";
+    private static final String M_M_GRECA =
+            "INSERT INTO tbl_pontoMuseusT (titulo,descricao,texto, latitude, longitude, idTab)" +
+                    "values ('Museu do Museu Murilo La Greca'," +
+                    "'R. Leonardo Bezerra Cavalcante,366, Parnamirim.'," +
+                    "' Comportando um acervo de 1.400 desenhos, com técnicas de fusain, crayon, pastel e sanguínea, o museu ainda possui discos, livros e mobiliários expostos em caráter de longa duração em quatro das oito salas de exposição. Há também 160 pinturas, entre paisagens, retratos e impressionistas e cartas, trocadas com Portinari e Giacometti. Foi nesse acervo que La Greca deixou sua marca inconfundível, tendo o seu forte na diversidade de sua obra artística, da aproximação com a arte contemporânea. Esse rico patrimônio está à disposição da população recifense, de forma atualmente mais intensa e orgânica. '," +"-8.037325,"+"-34.909832,"+"1"+
+            ");";
+    private static final String M_C_RECIFE=
+            "INSERT INTO tbl_pontoMuseusT (titulo,descricao,texto, latitude, longitude, idTab)" +
+                    "values ('Museu da Cidade do Recife'," +
+                    "'Praca das cinco pontas, São José.'," +
+                    "' O museu destaca-se por conter em seu acervo documentos iconográficos de extrema importância para preservação da história urbana e social do Recife. A memória cultural da capital pernambucana é representada através de cerca de 150 mil imagens e de peças provenientes de antigas residências e da Igreja do Senhor Bom Jesus dos Martírios, já demolida.'," +"-8.071313,"+"-34.880809,"+"1"+
+            ");";
+    private static final String M_MILITAR=
+            "INSERT INTO tbl_pontoMuseusT (titulo,descricao,texto, latitude, longitude, idTab)" +
+                    "values ('Museu Militar'," +
+                    "'Av. Militar, s/n, Recife'," +
+                    "' Em 1595 existia no local a Bateria Bom Jesus. Em 1629 foram iniciadas as obras de um novo forte. Após a invasão holandesa tomou o nome de Johann de Bruyne. Reconstruído após a Insurreição Pernambucana e concluído em 1690, abriga hoje o Museu Militar, com canhões portugueses/ingleses, armas, fotos e objetos que marcaram a ação da Força Aérea Brasileira na II Guerra Mundial.'," +"-8.052803,"+"-34.871412,"+"1"+
+            ");";
+    private static final String M_A_M_A_MAGALHAES=
+            "INSERT INTO tbl_pontoMuseusT (titulo,descricao,texto, latitude, longitude, idTab)" +
+                    "values ('Museu de Arte Moderna Aloísio Magalhães'," +
+                    "'Rua da Aurora, 265, Boa Vista'," +
+                    "'O MAMAM conta com um acervo de cerca de 1.100 trabalhos, de diversas técnicas, que abrangem um período histórico compreendido entre 1920 e 2008. Deste acervo, fazem parte obras fundamentais para a compreensão da arte moderna e contemporânea brasileira, de renomados artistas, dentre os quais se destacam João Câmara (com a série “Cenas da Vida Brasileira”), Vicente do Rego Monteiro (com um conjunto de telas que representa a maior coleção pública desse importante artista brasileiro), Aloisio Magalhães, Gil Vicente, Lula Cardoso Ayres, Abelardo da Hora, Joaquim do Rego Monteiro, Francisco Brennand, Gilvan Samico, José Patrício, Paulo Bruscky, Nelson Leirner, Alex Flemming, Carlos Fajardo, Marcelo Silveira, Rivane Neuenschwander, Ernesto Neto e Vik Muniz.'," +"-8.061285,"+"-34.881394,"+"1"+
+            ");";
+
+    private static final String ME_S_JOSE=
+            "INSERT INTO tbl_pontoMercados (titulo,descricao,texto, latitude, longitude, idTab)" +
+                    "values ('Mercado de São José'," +
+                    "' Praça Dom Vital, s/n - São José, Recife'," +
+                    "'O Mercado de São José ocupa uma área coberta de 3.541 metros quadrados. Mede 48,88 m de frente por 75,44 m de fundo. O prédio é formado por dois pavilhões, com 377 compartimentos de diversos produtos; 27 pedras de peixe; 34 barracas internas – para vender comidas e caldo de cana – e outras 70 espalhadas pela calçada do pátio. Atualmente, são 545 boxes no total. Artesanato em barro, corda e palha fazem do mercado polo de atração turística. É, também, ponto tradicional do comércio de pescado. Semanalmente são vendidos, ali, cerca de 1,3 toneladas de peixe e 400 kg de crustáceos.'," +"-8.06806,"+"-34.87775,"+"4"+
+            ");";
+
+    private static final String ME_B_VISTA=
+            "INSERT INTO tbl_pontoMercados (titulo,descricao,texto, latitude, longitude, idTab)" +
+                    "values ('Mercado da Boa Vista'," +
+                    "' R. de Santa Cruz, Boa Vista'," +
+                    " 'O Mercado da Boa Vista possui 63 boxes, que comercializam cereais, verduras, frutas e legumes, carnes, aves e frios, além de ervas e armarinhos. Há nove bares, que servem comida regional no café da manhã, almoço e jantar. A clientela é formada, principalmente, por sindicalistas e políticos, que se deliciam com o famoso patinho cozido no feijão preto.'," +"-8.063192,"+"-34.888654,"+"4"+
+            ");";
+
+    private static final String ME_MADALENA=
+            "INSERT INTO tbl_pontoMercados (titulo,descricao,texto, latitude, longitude, idTab)" +
+                    "values ('Mercado da Madalena'," +
+                    "'Rua Real da Torre, na praça Solange Pinto Melo, Madalena'," +
+                    " 'O Bairro da Madalena é local de grande importância histórica. Foi rentável zona açucareira do Recife antigo. A construção do mercado teve início em 6 de fevereiro de 1925, e a inauguração se deu no mesmo ano. No local, se reunía um aglomerado de feirantes, que ali vendiam frutas e verduras, sem qualquer interferência por parte da Prefeitura. Funcionava à noite e, por isso, recebeu o nome de Mercado do Bacurau. Hoje, são 180 compartimentos, que oferecem alimentos variados: frutas, verduras, legumes, cereais, carnes e peixe.'," +"-8.052439,"+"-34.908554,"+"4"+
+            ");";
+
+
+
+
+
 
 }
