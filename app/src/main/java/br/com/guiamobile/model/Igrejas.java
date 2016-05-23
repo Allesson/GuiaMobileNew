4c99349d73ab4aed99a213b9728ab3ae5d0d37cb
Allesson <allessoncavalcante@gmail.com>
2016-05-23 15:02:06 -0300

    Construção das tabelas do banco

---
+++ app/src/main/java/br/com/guiamobile/model/Igrejas.java
@@ -0,0 +1,49 @@
+package br.com.guiamobile.model;
+
+/**
+ * Created by Allesson on 23/05/2016.
+ */
+public class Igrejas {
+    protected static final String IGREJA1=
+            "INSERT INTO tbl_pontoIgrejasM (titulo,descricao,texto, latitude, longitude, idTab)" +
+                    "values (''," +
+                    "''," +
+                    " ''," +","+","+"2"+
+                    ");";
+    protected static final String IGREJA2=
+            "INSERT INTO tbl_pontoIgrejasM (titulo,descricao,texto, latitude, longitude, idTab)" +
+                    "values (''," +
+                    "''," +
+                    " ''," +","+","+"2"+
+                    ");";
+    protected static final String IGREJA3=
+            "INSERT INTO tbl_pontoIgrejasM (titulo,descricao,texto, latitude, longitude, idTab)" +
+                    "values (''," +
+                    "''," +
+                    " ''," +","+","+"2"+
+                    ");";
+    protected static final String IGREJA4=
+            "INSERT INTO tbl_pontoIgrejasM (titulo,descricao,texto, latitude, longitude, idTab)" +
+                    "values (''," +
+                    "''," +
+                    " ''," +","+","+"2"+
+                    ");";
+    protected static final String IGREJA5=
+            "INSERT INTO tbl_pontoIgrejasM (titulo,descricao,texto, latitude, longitude, idTab)" +
+                    "values (''," +
+                    "''," +
+                    " ''," +","+","+"2"+
+                    ");";
+    protected static final String IGREJA6=
+            "INSERT INTO tbl_pontoIgrejasM (titulo,descricao,texto, latitude, longitude, idTab)" +
+                    "values (''," +
+                    "''," +
+                    " ''," +","+","+"2"+
+                    ");";
+    protected static final String IGREJA7=
+            "INSERT INTO tbl_pontoIgrejasM (titulo,descricao,texto, latitude, longitude, idTab)" +
+                    "values (''," +
+                    "''," +
+                    " ''," +","+","+"2"+
+                    ");";
+}
---
+++ app/src/main/java/br/com/guiamobile/model/Mercados.java
@@ -0,0 +1,27 @@
+package br.com.guiamobile.model;
+
+/**
+ * Created by Allesson on 23/05/2016.
+ */
+public class Mercados {
+    protected static final String MERCADO1=
+            "INSERT INTO tbl_pontoMercados (titulo,descricao,texto, latitude, longitude, idTab)" +
+                    "values ('Mercado de São José'," +
+                    "'Praça Dom Vital, s/n - São José, Recife'," +
+                    "'O Mercado de São José ocupa uma área coberta de 3.541 metros quadrados. Mede 48,88 m de frente por 75,44 m de fundo. O prédio é formado por dois pavilhões, com 377 compartimentos de diversos produtos; 27 pedras de peixe; 34 barracas internas – para vender comidas e caldo de cana – e outras 70 espalhadas pela calçada do pátio. Atualmente, são 545 boxes no total. Artesanato em barro, corda e palha fazem do mercado polo de atração turística. É, também, ponto tradicional do comércio de pescado. Semanalmente são vendidos, ali, cerca de 1,3 toneladas de peixe e 400 kg de crustáceos.'," +"-8.06806,"+"-34.87775,"+"4"+
+                    ");";
+
+    protected static final String MERCADO2=
+            "INSERT INTO tbl_pontoMercados (titulo,descricao,texto, latitude, longitude, idTab)" +
+                    "values ('Mercado da Boa Vista'," +
+                    "' R. de Santa Cruz, Boa Vista'," +
+                    " 'O Mercado da Boa Vista possui 63 boxes, que comercializam cereais, verduras, frutas e legumes, carnes, aves e frios, além de ervas e armarinhos. Há nove bares, que servem comida regional no café da manhã, almoço e jantar. A clientela é formada, principalmente, por sindicalistas e políticos, que se deliciam com o famoso patinho cozido no feijão preto.'," +"-8.063192,"+"-34.888654,"+"4"+
+                    ");";
+
+    protected static final String MERCADO3=
+            "INSERT INTO tbl_pontoMercados (titulo,descricao,texto, latitude, longitude, idTab)" +
+                    "values ('Mercado da Madalena'," +
+                    "'Rua Real da Torre, na praça Solange Pinto Melo, Madalena'," +
+                    " 'O Bairro da Madalena é local de grande importância histórica. Foi rentável zona açucareira do Recife antigo. A construção do mercado teve início em 6 de fevereiro de 1925, e a inauguração se deu no mesmo ano. No local, se reunía um aglomerado de feirantes, que ali vendiam frutas e verduras, sem qualquer interferência por parte da Prefeitura. Funcionava à noite e, por isso, recebeu o nome de Mercado do Bacurau. Hoje, são 180 compartimentos, que oferecem alimentos variados: frutas, verduras, legumes, cereais, carnes e peixe.'," +"-8.052439,"+"-34.908554,"+"4"+
+                    ");";
+}
