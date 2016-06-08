0f4fefce5a8cd4761cafaf5cf9e916b581657e69
Allesson <allessoncavalcante@gmail.com>
2016-06-08 20:18:59 -0300

    Mudando cor da Splash screen

--- app/src/main/java/br/com/guiamobile/view/SplashActivity.java
+++ app/src/main/java/br/com/guiamobile/view/SplashActivity.java
@@ -20,7 +20,7 @@ public class SplashActivity extends Activity implements Runnable {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         splashScreen = new Handler();
-        splashScreen.postDelayed(SplashActivity.this, 600);
+        splashScreen.postDelayed(SplashActivity.this, 450);
     }
     @Override
     public void run() {
